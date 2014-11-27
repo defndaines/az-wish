a64202002fbf2b5233e9ae0427699aa343618411
defndaines <defndaines@gmail.com>
2014-11-27 12:26:08 -0800

    Bumping to latest alpha release.

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-alpha3"]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha4"]
                  [enlive "1.1.5"]])
