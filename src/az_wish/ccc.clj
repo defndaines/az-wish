c7c332129da00704dd799963e2c7fd320a04da35
defndaines <defndaines@gmail.com>
2014-12-21 21:46:46 -0800

    Initial foray into grabbing lowest price from ccc.

---
+++ src/az_wish/ccc.clj
@@ -0,0 +1,15 @@
+(ns core/ccc
+  (:require [net.cgrand.enlive-html :as html]))
+
+(defn- fetch-url [url]
+  (html/html-resource (java.net.URL. url)))
+
+;; http://camelcamelcamel.com/product/1941222226 ... redirects to ...
+;; http://camelcamelcamel.com/Mastering-Clojure-Macros-Cleaner-Smarter/product/1941222226
+
+(def direct (fetch-url "http://camelcamelcamel.com/product/1941222226"))
+
+(def az-lowest (second (html/select direct [:table.product_pane :tr.lowest_price :td])))
+;; And the next td is the date of that price
+
+(def price (-> az-lowest :content first))
