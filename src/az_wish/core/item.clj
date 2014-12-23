27e02d0e068715effccbf795a9908f135e8d42eb
defndaines <defndaines@gmail.com>
2014-12-22 21:49:57 -0800

    First pass scraping item price and availability.

---
+++ src/az_wish/core/item.clj
@@ -0,0 +1,24 @@
+(ns az-wish.core.item
+  (:require [net.cgrand.enlive-html :as html]))
+
+(defn- item-url [id]
+  (str "http://www.amazon.com/dp/" id "/"))
+
+(defn- fetch-url [url]
+  (html/html-resource (java.net.URL. url)))
+
+;; 193398838X ... http://www.amazon.com/dp/193398838X/
+
+(def dom (fetch-url (item-url "193398838X")))
+
+;; The price when currently being sold by Amazon.
+(-> (html/select dom [:span.offer-price]) first :content first)
+;; Sold by different party.
+(-> (html/select dom [:span#priceblock_ourprice]) first :content first)
+
+;; Availability:
+(-> (html/select dom [:div#availability :span]) first :content first clojure.string/trim)
+;; "In Stock."
+;; "This title will be released on MMMM dd, yyyy."
+;; "Only 2 left in stock."
+;; "Temporarily out of stock."
